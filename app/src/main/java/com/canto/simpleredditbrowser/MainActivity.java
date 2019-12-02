eda242d2a0c8d9b915245e0284daaf0496755eae
guillemollix <guillemollix@github.com>
2019-12-02 17:06:08 +0100

    Commit initial

---
+++ app/src/main/java/com/canto/simpleredditbrowser/MainActivity.java
@@ -0,0 +1,46 @@
+package com.canto.simpleredditbrowser;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Rends la toolbar en haut de l'écran réactive
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Rends le drawer accessible par un bouton dans la toolbar
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+
+    //Permet de fermer le drawer avec back lorsqu'il est ouvert
+    @Override
+    public void onBackPressed(){
+
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+}
